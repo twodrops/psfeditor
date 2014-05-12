f372aba568ddef0dba41e9662ba726c18b370a3c
Nirmal Sasidharan <nirmal.sasidharan@gmail.com>
2014-05-12 16:10:11 +0200

    Issue 23: Made IProject not IAdaptable. Project extends PlatformObject however

--- bundles/org.eclipselabs.team.psf.model/src/org/eclipselabs/team/psf/model/core/IProject.java
+++ bundles/org.eclipselabs.team.psf.model/src/org/eclipselabs/team/psf/model/core/IProject.java
@@ -1,25 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2012 Nirmal Sasidharan.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * Nirmal Sasidharan - initial API and implementation
+ * Copyright (c) 2012 Nirmal Sasidharan. All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Nirmal Sasidharan - initial API and implementation
  *******************************************************************************/
 package org.eclipselabs.team.psf.model.core;
 
 import java.util.List;
 
-import org.eclipse.core.runtime.IAdaptable;
-
 
 /**
  * Represents a project in Project Set model
+ * 
  * @author Nirmal Sasidharan
  */
-public interface IProject extends IAdaptable {
+public interface IProject {
 
   /**
    * @return the references
